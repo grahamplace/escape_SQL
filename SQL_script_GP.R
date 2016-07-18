6abb89570a48d7b678d58005516ac1c2df270e5f
Graham Place <grahamgplace@gmail.com>
2016-07-18 15:59:50 +0200

    create_dataBase method added

--- SQL_script_GP.R
+++ SQL_script_GP.R
@@ -1,2 +1,7 @@
-library(RSQLite)
-activity <- read.csv("activity.csv")
+create_dataBase <- function(event, food, activity) {
+  library(RSQLite)
+  dB <- dbConnect(SQLite(), "", cache_size = 5000, synchronous = "full")
+  dbWriteTable(dB, "event", event)
+  dbWriteTable(dB, "activity", activity)
+  dbWriteTable(dB, "food", food)
+}
