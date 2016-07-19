4ef745778711f56b152c3d90633306e53951be15
Graham Place <grahamgplace@gmail.com>
2016-07-19 13:20:14 +0200

    update to reflect change in db table structure

--- test_created_DB.R
+++ test_created_DB.R
@@ -1,8 +1,20 @@
 library(RSQLite)
 
+#conncet to database 
 db <- dbConnect(SQLite(), "escapeDB.sqlite")
+
+#check connection
 dbIsValid(db)
+
+#check fields of db tables 
 dbListFields(db, "activity")
 dbListFields(db, "food")
+
+#should throw error, no longer using event table 
 dbListFields(db, "event")
+
+#read in tables to check out: 
+activity <- dbReadTable(db, "activity")
+food <- dbReadTable(db, "food")
+
 
