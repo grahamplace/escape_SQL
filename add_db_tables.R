2e0a67239eeca1c5dd55e932d32b5bf11f95c23c
Graham Place <grahamgplace@gmail.com>
2016-07-19 13:41:20 +0200

    More db testing, add file to create function that adds tables to existing db

---
+++ add_db_tables.R
@@ -0,0 +1,4 @@
+#function to add scraped dataframes as SQL tables in database
+add_db_tables <- function(food, activity){
+  
+}
--- test_created_DB.R
+++ test_created_DB.R
@@ -1,7 +1,7 @@
 library(RSQLite)
 
 #conncet to database 
-db <- dbConnect(SQLite(), "escapeDB.sqlite")
+db <- dbConnect(SQLite(), "escapeDB.sqlite", cache_size = 5000, synchronous = "full")
 
 #check connection
 dbIsValid(db)
@@ -13,8 +13,18 @@ dbListFields(db, "food")
 #should throw error, no longer using event table 
 dbListFields(db, "event")
 
-#read in tables to check out: 
+#read in tables to check out
 activity <- dbReadTable(db, "activity")
 food <- dbReadTable(db, "food")
 
+#check class of each table's variables
+sapply(activity[,1:length(activity)], FUN = function(x) {class(x)})
+sapply(food[,1:length(food)], FUN = function(x) {class(x)})
 
+#try updating tables in R and writing to tables already in db 
+testAct <- as.data.frame(read_csv("activity.csv"))
+
+dbWriteTable(db, "activitytest", testAct)
+dbListFields(db, "activitytest")
+activitytest <- dbReadTable(db, "activitytest")
+sapply(activitytest[,1:length(activitytest)], FUN = function(x) {class(x)})
